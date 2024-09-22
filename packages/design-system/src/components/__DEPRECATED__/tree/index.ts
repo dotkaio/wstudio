9bcfddf99f2c14f41d0e3c6fc40e38dc77e6202e
dotkaio <github@kaio.email>
2024-09-21 21:22:58 -0700

    wstudio 1st commit

---
+++ packages/design-system/src/components/__DEPRECATED__/tree/index.ts
@@ -0,0 +1,9 @@
+export type { ItemSelector, ItemDropTarget } from "./item-utils";
+export * from "./tree";
+export {
+  TreeItemBody,
+  TreeItemLabel,
+  getNodeVars,
+  getPlacementIndicatorAlignment,
+  type TreeItemRenderProps,
+} from "./tree-node";
